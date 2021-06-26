86a83610ff5e929ca2f2aaca4dd604483f1c0b9b
javiddelossantos5 <javiddelossantos5@gmail.com>
2021-06-26 16:28:53 -0600

    adding json web tokens to hash password and user so we can grab it for authentication using a middleware

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const gravatar = require('gravatar');
 const { check, validationResult } = require('express-validator/check');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const User = require('../../models/User');
 
@@ -57,7 +59,20 @@ async (req, res) => {
         await user.save();
 
         // Return jsonwebtoken
-        res.send('User Registered');
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(
+            payload, 
+            config.get('jwtSecret'),
+            { expiresIn: 3600 },
+            (err, token) => {
+                if (err) throw err;
+                res.json({ token });
+            });
 
     } catch (err) {
         console.error(err.message);
