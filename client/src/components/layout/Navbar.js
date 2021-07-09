17008ae3d61bffbca03af03fe9360f575fc42b06
Javid De Los Santos <javiddelossantos5@gmail.com>
2021-07-09 08:27:02 -0600

    finished authentication with JWT and local storage now gonna work on dashboard

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logout } from '../../actions/auth';
 
-const Navbar = () => {
-  return (
-    <nav className='navbar bg-dark'>
-      <h1>
-        <Link to='/'>
-          {/* <i className='fas fa-code'></i>  */}
-          Developer Connector
-        </Link>
-      </h1>
-      <ul>
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+  const authLinks = (
+    <ul>
         <li>
-          <Link to='/profiles'>Developers</Link>
+          <a onClick={logout} to='#!'>
+            <span className='hide-sm'>Logout</span>
+          </a>
+        </li>
+    </ul>
+  );
+
+  const guestLinks = (
+    <ul>
+        <li>
+          <Link to='#!'>Developers</Link>
         </li>
         <li>
           <Link to='/register'>Register</Link>
@@ -21,8 +27,29 @@ const Navbar = () => {
           <Link to='/login'>Login</Link>
         </li>
       </ul>
+  );
+
+  return (
+    <nav className='navbar bg-dark'>
+      <h1>
+        <Link to='/'>
+          {/* <i className='fas fa-code'></i>  */}
+          Developer Connector
+        </Link>
+      </h1>
+      {/* make sure loading is off before showing the menu bar */}
+      { !loading && (<Fragment>{isAuthenticated ? authLinks: guestLinks }</Fragment>) }
     </nav>
   );
 };
 
-export default Navbar;
+Navbar.protoType = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, { logout })(Navbar);
