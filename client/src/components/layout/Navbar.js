a37087fc2e60600519aae6def1ad4142a697f39e
Javid De Los Santos <javiddelossantos5@gmail.com>
2021-07-13 14:26:26 -0600

    added posts so developers can comment like and talk to other developers

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -13,6 +13,11 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
         </Link>
       </li>
       <li>
+        <Link to='/posts'>
+          Posts
+        </Link>
+      </li>
+      <li>
         <Link to='/dashboard'>
           <span className='hide-sm'>Dashboard</span>
         </Link>
