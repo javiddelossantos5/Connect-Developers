eaa644cec986f85f8968d53a6f43f0a2a61473cc
Javid De Los Santos <javiddelossantos5@gmail.com>
2021-07-13 13:17:49 -0600

    i had a huge bug with pulling from github api good now fixed it and finished profile and edits to profile UI

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -7,11 +7,25 @@ import { getGithubRepos } from '../../actions/profile';
 const ProfileGithub = ({ username, getGithubRepos, repos }) => {
     useEffect(() => {
         getGithubRepos(username);
-    }, [getGithubRepos(username)])
+    }, [getGithubRepos])
 
     return (
-        <div>
-            
+        <div className='profile-github'>
+            <h2 className="text-primary my-1">Github Repos</h2>
+            {repos === null ? <Spinner /> : (
+                repos.map((repo) => (
+                    <div key={repo._id} className='repo bg-white p-1 my-1'>
+                        <div>
+                            <h4>
+                                <a href={repo.html_url} target='_blank' rel='noopener noreferrer'>
+                                    {repo.name}
+                                </a>
+                            </h4>
+                            <p>{repo.description}</p>
+                        </div>
+                    </div>
+                ))
+            )}
         </div>
     )
 };
